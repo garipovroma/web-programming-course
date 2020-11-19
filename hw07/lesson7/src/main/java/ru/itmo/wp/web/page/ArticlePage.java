0c5740b0bac3aaacba37f3031b5d3a11ee5784ce
Roma <devilgar@gmail.com>
2020-11-20 01:57:27 +0300

    hw7 partially completed

--- hw07/lesson7/src/main/java/ru/itmo/wp/web/page/ArticlePage.java
+++ hw07/lesson7/src/main/java/ru/itmo/wp/web/page/ArticlePage.java
@@ -1,12 +1,9 @@
 package ru.itmo.wp.web.page;
 
-import com.google.common.base.Strings;
-import org.checkerframework.checker.units.qual.A;
 import ru.itmo.wp.model.domain.Article;
 import ru.itmo.wp.model.domain.User;
 import ru.itmo.wp.model.exception.ValidationException;
 import ru.itmo.wp.model.service.ArticleService;
-import ru.itmo.wp.model.service.UserService;
 import ru.itmo.wp.web.exception.RedirectException;
 
 import javax.servlet.http.HttpServletRequest;
@@ -14,20 +11,15 @@ import java.util.Map;
 
 public class ArticlePage {
     private final ArticleService articleService = new ArticleService();
-    private final UserService userService = new UserService();
     private void action(HttpServletRequest request, Map<String, Object> view) {
         // No operations.
-        if (request.getSession().getAttribute("user") == null) {
-            request.getSession().setAttribute("message", "Talks available only for logged in users");
-            request.getSession().setAttribute("messageType", "error");
-            throw new RedirectException("/index");
-        }
+        checkLoggedInUser(request);
     }
     //:TODO: correct validation
 
     private void createArticle(HttpServletRequest request, Map<String, Object> view) throws ValidationException {
-        articleService.validateArticle(request);
         User user = (User) request.getSession().getAttribute("user");
+        articleService.validateArticleAndUser(request, user);
         Article article = new Article();
         article.setUserId(user.getId());
         article.setTitle((String) request.getParameter("title"));
@@ -35,5 +27,12 @@ public class ArticlePage {
         articleService.create(article);
         request.getSession().setAttribute("message", "You are successfully created the article!");
         throw new RedirectException("/index");
+    }
+    private void checkLoggedInUser(HttpServletRequest request) {
+        if (request.getSession().getAttribute("user") == null) {
+            request.getSession().setAttribute("message", "This page is available only for logged-in users");
+            request.getSession().setAttribute("messageType", "error");
+            throw new RedirectException("/index");
+        }
     }
 }
--- hw07/lesson7/src/main/webapp/js/app.js
+++ hw07/lesson7/src/main/webapp/js/app.js
@@ -1,23 +1,31 @@
-window.notify = function (message) {
+window.notify = function (message, className) {
+    if (className === undefined) {
+        className = "success";
+    }
     $.notify(message, {
         position: "right bottom",
-        className: "success"
+        className: className
     });
 }
 
-function ajax(data, context) {
+function ajax(data, context, successFunction) {
+    if (successFunction === undefined) {
+        successFunction = function (response) {
+            if (response["error"]) {
+                $(context).find(".error").text(response["error"]);
+            } else {
+                if (response["redirect"]) {
+                    location.href = response["redirect"];
+                }
+            }
+        };
+    }
     $.ajax({
         type: "POST",
         url: "",
         dataType: "json",
         data,
-        success: function (response) {
-            if (response["error"]) {
-                $(context).find(".error").text(response["error"]);
-            } else {
-                location.href = response["redirect"];
-            }
-        }
+        success: successFunction
     });
     return false;
 }
